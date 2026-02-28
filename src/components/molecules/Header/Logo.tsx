b032ffa876f090011c35bce436fb284611f9c01d
TotoRamos <tomasagustinramos@gmail.com>
2026-02-28 14:55:41 -0300

    Modificacion de ruta

--- src/components/molecules/Header/Logo.tsx
+++ src/components/molecules/Header/Logo.tsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-import logoMtr from "../../../assets/logo-mtr.png";
+import mtrlogo from "../../../../assets/logo-mtr.png";
 
 
 const Logo = () => (
   <Link  to="/" className="flex items-center gap-2 hover:opacity-80 transition-opacity">
     <div className="w-10 h-10  from-blue-500 to-purple-600 rounded-lg flex items-center justify-center">
-      <img src={logoMtr} alt="MTR Logo" className="w-6 h-6" />
+      <img src={mtrlogo} alt="MTR Logo" className="w-6 h-6" />
     </div>
     <span className="text-xl font-bold text-gray-800">MTR</span>
   </Link>
